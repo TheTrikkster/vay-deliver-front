6957244c6989a093c2ea922c3d6993b05869fd33
bmoby <msouleymanov@gmail.com>
2025-05-19 20:58:50 +0700

    test prod api

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestHeaders } from 'axios';
 import { handleApiError } from '../utils/errorHandling';
 import { fetchAuthSession } from 'aws-amplify/auth';
 
-export const API_BASE_URL_DEV = 'http://localhost:3300';
+export const API_BASE_URL_DEV = 'https://bvgxoado1l.execute-api.us-east-1.amazonaws.com/';
 export const API_BASE_URL_PROD = 'https://bvgxoado1l.execute-api.us-east-1.amazonaws.com/';
 const api = axios.create({
   baseURL: API_BASE_URL_DEV,
@@ -16,11 +16,12 @@ api.interceptors.request.use(
   async config => {
     try {
       const session = await fetchAuthSession();
-      const idToken = session.tokens?.idToken?.toString();
-      console.log(idToken);
-      if (idToken) {
+      const accessToken = session.tokens?.accessToken?.toString();
+      console.log('AccessToken:', accessToken);
+      if (accessToken) {
         if (!config.headers) config.headers = {} as AxiosRequestHeaders;
-        (config.headers as any)['Authorization'] = `Bearer ${idToken}`;
+        (config.headers as any)['Authorization'] = `Bearer ${accessToken}`;
+        console.log('Headers:', config.headers);
       }
     } catch (error) {
       // Pas d'utilisateur connecté ou erreur de récupération du token
@@ -35,8 +36,21 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   response => response,
   error => {
-    const errorMessage = handleApiError(error);
-    console.error(errorMessage);
+    if (error.response) {
+      // La requête a été faite et le serveur a répondu avec un code d'état
+      // qui est en dehors de la plage 2xx
+      console.error('Response error:', {
+        status: error.response.status,
+        data: error.response.data,
+        headers: error.response.headers,
+      });
+    } else if (error.request) {
+      // La requête a été faite mais aucune réponse n'a été reçue
+      console.error('Request error:', error.request);
+    } else {
+      // Une erreur s'est produite lors de la configuration de la requête
+      console.error('Error:', error.message);
+    }
     return Promise.reject(error);
   }
 );
