14e858db9feac4980b2ad9324e02c8e572fdacaa
Israil <israilst67@gmail.com>
2025-05-19 18:21:20 +0200

    update error api config

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestHeaders } from 'axios';
 import { handleApiError } from '../utils/errorHandling';
 import { fetchAuthSession } from 'aws-amplify/auth';
 
-export const API_BASE_URL_DEV = 'https://bvgxoado1l.execute-api.us-east-1.amazonaws.com/';
+export const API_BASE_URL_DEV = 'http://localhost:3300';
 export const API_BASE_URL_PROD = 'https://bvgxoado1l.execute-api.us-east-1.amazonaws.com/';
 const api = axios.create({
   baseURL: API_BASE_URL_DEV,
@@ -24,8 +24,9 @@ api.interceptors.request.use(
         console.log('Headers:', config.headers);
       }
     } catch (error) {
-      // Pas d'utilisateur connecté ou erreur de récupération du token
-      console.log(error);
+      const errorMessage = handleApiError(error);
+      console.error(errorMessage);
+      return Promise.reject(error);
     }
     return config;
   },
@@ -36,21 +37,8 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   response => response,
   error => {
-    if (error.response) {
-      // La requête a été faite et le serveur a répondu avec un code d'état
-      // qui est en dehors de la plage 2xx
-      console.error('Response error:', {
-        status: error.response.status,
-        data: error.response.data,
-        headers: error.response.headers,
-      });
-    } else if (error.request) {
-      // La requête a été faite mais aucune réponse n'a été reçue
-      console.error('Request error:', error.request);
-    } else {
-      // Une erreur s'est produite lors de la configuration de la requête
-      console.error('Error:', error.message);
-    }
+    const errorMessage = handleApiError(error);
+    console.error(errorMessage);
     return Promise.reject(error);
   }
 );
