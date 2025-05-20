6b6a2294e66db585c90842e37f792c6cf6f52a30
Israil <israilst67@gmail.com>
2025-05-20 12:38:10 +0200

    replace variable with window.location.origin

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -26,7 +26,7 @@ type Config = {
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.REACT_APP_API_BASE_URL_PROD!, window.location.href);
+    const publicUrl = new URL(window.location.origin, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if REACT_APP_API_BASE_URL_PROD is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -35,7 +35,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.REACT_APP_API_BASE_URL_PROD}/service-worker.js`;
+      const swUrl = `${window.location.origin}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
